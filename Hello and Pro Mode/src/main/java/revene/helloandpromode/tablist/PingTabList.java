49a878776687647394a73430660376df341d9015
tealtwo <80123588+tealtwo@users.noreply.github.com>
2023-05-16 14:28:17 -0500

    Add files via upload

---
+++ Hello and Pro Mode/src/main/java/revene/helloandpromode/tablist/PingTabList.java
@@ -0,0 +1,37 @@
+package revene.helloandpromode.tablist;
+
+import revene.helloandpromode.SpigotPing;
+import revene.helloandpromode.utils.PingUtil;
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
+import org.bukkit.scheduler.BukkitRunnable;
+
+public class PingTabList extends BukkitRunnable {
+    private SpigotPing plugin;
+
+    public PingTabList(SpigotPing plugin) {
+        this.plugin = plugin;
+    }
+
+    public void run() {
+        for (Player player : this.plugin.getServer().getOnlinePlayers()) {
+            String currentName;
+            if (plugin.getConfig().getBoolean("tablist.show-real-name")) {
+                currentName = player.getName();
+            } else {
+                currentName = player.getDisplayName();
+            }
+            String prefix = plugin.getConfig().getString("tablist.prefix");
+            if (!prefix.equals("")) {
+                player.setPlayerListName(ChatColor.translateAlternateColorCodes('&',
+                        prefix.replace("%ping%", "" + PingUtil.getPing(player))) + " " + currentName);
+            }
+            String suffix = plugin.getConfig().getString("tablist.suffix");
+            if (!suffix.equals("")) {
+                player.setPlayerListName(currentName + " " + ChatColor.translateAlternateColorCodes('&',
+                        suffix.replace("%ping%", "" + PingUtil.getPing(player))));
+            }
+        }
+    }
+}
+
---
+++ Hello and Pro Mode/src/main/java/revene/helloandpromode/utils/SoundUtil.java
@@ -0,0 +1,20 @@
+package revene.helloandpromode.utils;
+
+import revene.helloandpromode.SpigotPing;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.Sound;
+import org.bukkit.entity.Player;
+
+public class SoundUtil {
+    public static void playSound(Player p, String soundType) {
+        float volume = Float.parseFloat(SpigotPing.getInstance().getConfig().getString("sound-manager.volume"));
+        float pitch = Float.parseFloat(SpigotPing.getInstance().getConfig().getString("sound-manager.pitch"));
+        try {
+            p.playSound(p.getLocation(), Sound.valueOf(soundType), volume, pitch);
+        } catch (IllegalArgumentException e) {
+            Bukkit.getServer().getConsoleSender().sendMessage(ChatColor.RED + "[SpigotPing] Can not play a sound. " +
+                    "The specified sound-type is not available in your server version, please ensure to read carefully the config file.");
+        }
+    }
+}
